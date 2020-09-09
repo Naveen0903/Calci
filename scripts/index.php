08c111067281dc201c73c3ad6860481f5fab6742
Naveen <naveen@connectecho.in>
2020-09-09 13:32:34 +0530

    input evaluation

--- scripts/index.php
+++ scripts/index.php
@@ -1,26 +1,25 @@
 <?php
     $ma = $_GET["expression"];
-    $p=0;
+    $p= eval("return $ma ;");
     
-    if(preg_match('/(\d+)(?:\s*)([\+\-\*\/])(?:\s*)(\d+)/', $ma, $matches) !== FALSE){
-        $operator = $matches[2];
+    // if(preg_match('/(\d+)(?:\s*)([\+\-\*\/])(?:\s*)(\d+)/', $ma, $matches) !== FALSE){
+    //     $operator = $matches[2];
     
-        switch($operator){
-            case '+':
-                $p = $matches[1] + $matches[3];
-                break;
-            case '-':
-                $p = $matches[1] - $matches[3];
-                break;
-            case '*':
-                $p = $matches[1] * $matches[3];
-                break;
-            case '/':
-                $p = $matches[1] / $matches[3];
-                break;
-        }
+    //     switch($operator){
+    //         case '+':
+    //             $p = $matches[1] + $matches[3];
+    //             break;
+    //         case '-':
+    //             $p = $matches[1] - $matches[3];
+    //             break;
+    //         case '*':
+    //             $p = $matches[1] * $matches[3];
+    //             break;
+    //         case '/':
+    //             $p = $matches[1] / $matches[3];
+    //             break;
+    //     }
     
         // echo "<script>document.querySelector('.calculator-screen').value = $p</script>";
-        echo $p;
-    }
+    echo $p;
 ?>
